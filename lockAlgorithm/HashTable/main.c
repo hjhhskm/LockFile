a9ddef6f3c0b33f9d29dd4c375fe7e0f16376530
hanjiahui <947827078@qq.com>
2017-11-11 14:01:30 +0800

    add some test for main.c and test for the commit

--- lockAlgorithm/HashTable/main.c
+++ lockAlgorithm/HashTable/main.c
@@ -7,13 +7,13 @@ int main()
 	UINT32 length = 101;
 	HashTable_init(table,length);
 	HTAdd("litao",98,table);
-	HTAdd("wangbiaoyu",77,table);
-	HTAdd("lijipeng",85,table);
-	HTAdd("hanjiahui",83,table);
+	HTAdd("wangzhaoyang",83,table);
+    HTAdd("hanjiahui",83,table);
 	printf("litao's scroce is %d ",HTGet("litao",table));
 	HTDis(table);
 	printf("delete wangbiaoyu %d",HTDel("wangbiaoyu",table));
 	printf("delete wangbiaoyu %d",HTDel("wangbiaoyu",table));
-	HTDis(table);
+	printf("\r\n now is to display the table \r\n");
+    HTDis(table);
 	return 0;
 }
